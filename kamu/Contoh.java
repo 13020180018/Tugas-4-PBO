8ba26331c37a40d21ea91acd81e5db005bb41156
13020180018 <62000318+13020180018@users.noreply.github.com>
2020-03-28 15:43:04 +0700

    Add files via upload

---
+++ kamu/Contoh.java
@@ -0,0 +1,13 @@
+public class Contoh {
+
+      public static void main(String[] args) {
+        boolean kondisi= true;
+
+        if (kondisi){
+            System.out.println("Jawaban anda benar...");
+        }
+        else {
+            System.out.println("Jawaban anda benar salah...");
+        }
+    }
+}
